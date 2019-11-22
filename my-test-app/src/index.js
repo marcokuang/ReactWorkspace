82a02d5c364a963670d41c12715f0528ab97fb57
Marco K <marcokuang@users.noreply.github.com>
2019-11-21 23:24:24 -0800

    installed faker.js and make sub-components

--- my-test-app/src/index.js
+++ my-test-app/src/index.js
@@ -1,12 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import CommentDetail from './CommentDetail.js';
+
 
 const App = () => {
-  const buttonName = 'Click Me!';
-  return <div>
-    <p style={{backgroundColor: 'yellow', color: 'red'}}>This is an app</p>
-<button> {buttonName} </button>
-  </div>;
+  const commentExample = [{
+    author: 'Marco K',
+    time: 'Today at 7pm',
+    text: 'Awesome!!'
+  },
+  {
+    author: 'Mary Z',
+    time: 'Today at 4pm',
+    text: 'Nice Pics!!'
+  },
+  {
+    author: 'Jane O',
+    time: 'Today at 2pm',
+    text: 'This is legendary!!'
+  },
+  {
+    author: 'Erika Q',
+    time: 'Today at 12pm',
+    text: 'I like it a lot!'
+  }
+]
+    //   comment layout has 2 major components: 
+    // 1. avatar
+    // 2. content section
+    //    a. author name -> meta data
+    //    b. comment date
+    //    c. comment text
+  return (
+  <div className='ui container comments'>  
+    {
+      commentExample.map(comment => {
+        return <CommentDetail author={comment.author} timeAgo={comment.time} text={comment.text}/>
+      })
+    }
+  </div>);
+  
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
