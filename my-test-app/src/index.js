bf021f38e0ca0fbfe5bf0e4fda046c992a8178bd
Marco Kuang <marcokuang@Marcos-MacBook-Pro.local>
2020-01-19 22:28:22 -0800

    fix approvalcard issues

--- my-test-app/src/index.js
+++ my-test-app/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import CommentDetail from './CommentDetail.js';
+import Faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 
 
 const App = () => {
@@ -22,24 +24,32 @@ const App = () => {
   {
     author: 'Erika Q',
     time: 'Today at 12pm',
-    text: 'I like it a lot!'
+    text: 'I like it a lot!!'
   }
-]
-    //   comment layout has 2 major components: 
-    // 1. avatar
-    // 2. content section
-    //    a. author name -> meta data
-    //    b. comment date
-    //    c. comment text
+  ]
+  //   comment layout has 2 major components: 
+  // 1. avatar
+  // 2. content section
+  //    a. author name -> meta data
+  //    b. comment date
+  //    c. comment text
   return (
-  <div className='ui container comments'>  
-    {
-      commentExample.map(comment => {
-        return <CommentDetail author={comment.author} timeAgo={comment.time} text={comment.text}/>
-      })
-    }
-  </div>);
-  
+    <div className='ui container comments'>
+      <ApprovalCard></ApprovalCard>
+      {
+        commentExample.map(comment => {
+          return (
+            <CommentDetail
+              author={comment.author}
+              timeAgo={comment.time}
+              content={comment.text}
+              avatar={Faker.image.avatar()}
+            />
+          );
+        })
+      }
+    </div>);
+
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
