774098cb767e674536a02859acd55f74cddf78ef
MarcoK <marcojkuang@gmail.com>
2020-01-21 20:33:19 -0800

    add error handling and conditional rendering

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -1,28 +1,44 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
-    this.state = {lat: null};
+    this.state = { lat: null, errorMsg: "" };
 
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
+      position => {
         console.log(position);
-        this.setState({lat: position.coords.latitude});
+        this.setState({ lat: position.coords.latitude });
       },
-      err => console.error(err)
+      err => {
+        this.setState({ errorMsg: err.message });
+      }
     );
   }
 
-  render(){
-    return (
-      <div className="App">
+  render() {
+    if (!this.state.lat && !this.state.errorMsg) {
+      return (
+        <div className="App">
+          <h1>Loading...</h1>
+        </div>
+      );
+    } else if (this.state.lat) {
+      return (
+        <div className="App">
           <h1>Lattitude: {this.state.lat}</h1>
-      </div>
-    );
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <h1>Error: {this.state.errorMsg}</h1>
+        </div>
+      );
     }
+  }
 }
 
 export default App;
