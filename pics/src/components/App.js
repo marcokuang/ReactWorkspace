a75de952f6e37abe917d5949daba9ce0f1071eb0
MarcoK <marcojkuang@gmail.com>
2020-02-02 17:29:11 -0800

    finish setting up callback fn on App Component

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,10 +3,20 @@ import SearchBar from "./SearchBar";
 import "../css/App.css";
 
 class App extends React.Component {
+  state = {
+    term: ""
+  };
+
+  onSearchSubmit(term) {
+    console.log(`App Component >>> ${term}`);
+    this.setState({ term: term });
+  }
+
   render() {
     return (
       <div className="ui container app-container">
-        <SearchBar />
+        <SearchBar onSubmit={this.onSearchSubmit.bind(this)} />
+        <div>Keyword: {this.state.term}</div>
       </div>
     );
   }
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -4,20 +4,27 @@ class SearchBar extends React.Component {
   state = { term: "" };
 
   onInputChange(event) {
-    console.log(event.target.value);
     this.setState({ term: event.target.value });
   }
+
+  // use arrow function to make sure the 'this' keyword will always assign to SearchBar class
+  // this is the same as binding the 'this' keyword in the event handler below in render()
+  onFormSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
             <input
               type="text"
               placeholder="Search Bar..."
               onChange={this.onInputChange.bind(this)}
+              value={this.state.term}
             />
           </div>
         </form>
