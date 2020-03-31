f94bac2c05ddb1eb2cb2ebd5aad6e492e9a238a6
Marco Kuang <marcojkuang@gmail.com>
2020-03-30 19:29:16 -0700

    add axios api

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions";
 
 class PostList extends React.Component {
-    render(){
-        return <div className="ui list">PostList</div>
-    }
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return <div className="ui list">PostList</div>;
+  }
 }
 
-export default PostList;
+export default connect(null, { fetchPosts })(PostList);
