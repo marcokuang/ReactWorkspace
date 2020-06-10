b8fb2733fbdef0c41b5be202c01e06d652c1828e
Marco Kuang <marcojkuang@gmail.com>
2020-06-10 12:32:06 -0700

    finish the streaming functionality with OBS studio

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,11 +1,46 @@
 import React from "react";
 import { connect } from "react-redux";
+import flv from "flv.js";
 import { fetchStream } from "../../actions";
 
 class StreamShow extends React.Component {
-  componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
   }
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  // it will be called after the component updates
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  // clean up logics are here
+  componentWillUnmount() {
+    console.log("component was unmounted");
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    const { id } = this.props.match.params;
+    // if this.player is already set up or the stream does not exist
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  }
+
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
@@ -13,6 +48,7 @@ class StreamShow extends React.Component {
     const { title, description } = this.props.stream;
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls></video>
         <h2>{title}</h2>
         <p>{description}</p>
       </div>
