7a410f5ad66e79b2237763e6a83517784707ea11
Marco Kuang <marcojkuang@gmail.com>
2020-06-13 18:05:57 -0700

    clean up the seasons app

--- seasons/src/hook/App.js
+++ seasons/src/hook/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "../App.css";
 import SeasonDisplay from "../SeasonDisplay";
 import Spinner from "../Spinner";
