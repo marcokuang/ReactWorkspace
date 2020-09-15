0b6f0ae437dc713802fc7164025e647619d1152d
Marco K <marcok@Marcos-MacBook-Air.local>
2020-09-15 16:19:24 -0700

    add csv practice in ts

--- ts-csv-stat/dist/index.js
+++ ts-csv-stat/dist/index.js
@@ -1,2 +1,17 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+var csv = fs_1.default.readFileSync("src/football.csv", {
+    encoding: "utf-8",
+});
+var loadData = function (csv) {
+    var res = csv.split("\n").map(function (row) {
+        return row.split(",");
+    });
+    return res;
+};
+console.log(JSON.stringify(loadData(csv)));
 console.log("hellow___");
--- ts-csv-stat/src/index.ts
+++ ts-csv-stat/src/index.ts
@@ -1,1 +1,20 @@
+import { readFileSync } from "fs";
+
+import fs from "fs";
+import path from "path";
+
+const csv = fs.readFileSync("src/football.csv", {
+  encoding: "utf-8",
+});
+
+const loadData = (csv: string): string[][] => {
+  const res: string[][] = csv.split("\n").map((row: string): string[] => {
+    return row.split(",");
+  });
+
+  return res;
+};
+
+console.log(JSON.stringify(loadData(csv)));
+
 console.log("hellow___");
